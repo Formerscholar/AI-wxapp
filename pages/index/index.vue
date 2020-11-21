d6647ad4715f29ed1135154f23fb9fd4919e82e7
ChunMing <82867782@qq.com>
2020-11-21 19:29:36 +0800

    update pages/index/index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -203,13 +203,6 @@
 					iconPath: '/static/imgs/icon/myPaper1.png',
 					selectedIconPath: '/static/imgs/icon/myPaper.png'
 				});
-				uni.login({
-					success: (res) => {
-						this.$api.teacher_login({
-							code: res.code
-						})
-					}
-				})
 			} else {
 				uni.setTabBarItem({
 					index: 1,
@@ -217,13 +210,6 @@
 					iconPath: '/static/imgs/icon/cuotiben1.png',
 					selectedIconPath: '/static/imgs/icon/cuotiben.png'
 				});
-				uni.login({
-					success: (res) => {
-						this.$api.student_login({
-							code: res.code
-						})
-					}
-				})
 			}
 			this.get_banner();
 			if (this.token && this.type == 3) {
@@ -232,6 +218,24 @@
 			if (this.token && this.type == 4) {
 				this.get_student();
 			}
+			let _this = this;
+			// 查看是否授权
+			wx.getSetting({
+			  success (res){
+				if (res.authSetting['scope.userInfo']) {
+				  // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+				  wx.getUserInfo({
+					success: function(res) {
+					  console.log(res.userInfo)
+					  res.openid = uni.getStorageSync('userInfo').openid;
+					  _this.$api.update_userinfo(res).then(res => {
+						console.log(res);
+					  });
+					}
+				  })
+				}
+			  }
+			})
 		},
 		onShareAppMessage() {
 			return {
