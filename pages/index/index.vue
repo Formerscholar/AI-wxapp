0daa582d4f581eca01379c9f6dcf0c892b042bbd
ChunMing <82867782@qq.com>
2020-12-19 13:28:32 +0800

    update pages/index/index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -225,14 +225,13 @@
 			if (this.token && this.type == 4) {
 				this.get_student();
 			}
-			// #ifdef MP-WEIXIN
 			let _this = this;
 			// 查看是否授权
-			wx.getSetting({
+			uni.getSetting({
 				success(res) {
 					if (res.authSetting['scope.userInfo']) {
 						// 已经授权，可以直接调用 getUserInfo 获取头像昵称
-						wx.getUserInfo({
+						uni.getUserInfo({
 							success: function(res) {
 								console.log(res.userInfo)
 								res.openid = uni.getStorageSync('userInfo').openid;
@@ -241,10 +240,17 @@
 								});
 							}
 						})
+					}else{
+						uni.login({
+							success: (res) => {
+								_this.$api.wx_login(res).then(res => {
+									console.log(res);
+								});
+							}
+						})
 					}
 				}
 			})
-			// #endif
 		},
 		onShareAppMessage() {
 			return {
